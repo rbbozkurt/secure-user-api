b3928390e4d0584d0b6534fb1f79a55d96ef73a1
Resit Berkay Bozkurt <rbbozkurt@Resits-MacBook-Pro.local>
2025-04-22 10:46:15 +0200

    git init

---
+++ src/main/kotlin/com/rbbozkurt/springbootdemo/SpringbootDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.rbbozkurt.springbootdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootDemoApplication>(*args)
+}
